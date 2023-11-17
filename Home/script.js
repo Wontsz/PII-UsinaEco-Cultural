25705dcc61cb561ba89f952f557731345c366678
bleer <125562229+bleer2004@users.noreply.github.com>
2023-11-17 17:47:04 -0300

    site pronto uhuuuu, falta só colocar foto

--- Home/script.js
+++ Home/script.js
@@ -1,60 +1,7 @@
-var basicTimeline = anime.timeline({
-  autoplay: false
-});
+const carousel = document.querySelector(".carousel-container");
+const slide = document.querySelector(".carousel-slide");
 
-var pathEls = $(".check");
-for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
-  pathEl.setAttribute("stroke-dashoffset", offset);
+function handleCarouselMove(positive = true) {
+  const slideWidth = slide.clientWidth;
+  carousel.scrollLeft = positive ? carousel.scrollLeft + slideWidth : carousel.scrollLeft - slideWidth;
 }
-
-basicTimeline
-  .add({
-    targets: ".text-assinar",
-    duration: 1,
-    opacity: "0"
-  })
-  .add({
-    targets: ".button-assinar",
-    duration: 1300,
-    height: 10,
-    width: 300,
-    backgroundColor: "#2B2D2F",
-    border: "0",
-    borderRadius: 100
-  })
-  .add({
-    targets: ".progress-bar",
-    duration: 2000,
-    width: 300,
-    easing: "linear"
-  })
-  .add({
-    targets: ".button-assinar",
-    width: 0,
-    duration: 1
-  })
-  .add({
-    targets: ".progress-bar",
-    width: 80,
-    height: 80,
-    delay: 500,
-    duration: 750,
-    borderRadius: 80,
-    backgroundColor: "#71DFBE"
-  })
-  .add({
-    targets: pathEl,
-    strokeDashoffset: [offset, 0],
-    duration: 200,
-    easing: "easeInOutSine"
-  });
-
-$(".button-assinar").click(function() {
-  basicTimeline.play();
-});
-
-$(".text-assinar").click(function() {
-  basicTimeline.play();
-});
